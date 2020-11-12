50f4daa0dcb2ff2cd258eb88568fa79502043d35
Albert Zeyer <albzey@gmail.com>
2020-11-12 16:26:08 +0100

    more

--- pytorch_to_returnn/import_wrapper/torch_wrappers/tensor.py
+++ pytorch_to_returnn/import_wrapper/torch_wrappers/tensor.py
@@ -3,8 +3,25 @@ import torch
 from ... import log
 
 
+# See the discussion here, which is very relevant,
+# which covers exactly the topic how one could potentially wrap `torch.Tensor`.
+# https://github.com/pytorch/pytorch/blob/master/docs/source/notes/extending.rst#extending-modtorch-with-a-classtensor-like-type
+
+# A soft proxy type unfortunately does not work,
+# because `isinstance(obj, torch.Tensor)` checks would not work correctly,
+# and they are used in `torch.nn.Module`.
+
+
 class WrappedTorchTensor(torch.Tensor):  # TODO
   def __getattribute__(self, item):
-    if log.Verbosity >= 6:
-      log.unique_print("**** torch tensor __getattribute__ %r" % item)
+    if log.Verbosity >= 10:
+      log.unique_print(f"**** torch tensor __getattribute__ {item!r}")
     return super(WrappedTorchTensor, self).__getattribute__(item)
+
+  @classmethod
+  def __torch_function__(cls, func, types, args=(), kwargs=None):
+    if log.Verbosity >= 6 and func not in {torch.Tensor.__repr__, torch.Tensor.__str__}:
+      log.unique_print(f"**** torch tensor func {func.__name__}")
+    if kwargs is None:
+      kwargs = {}
+    return super().__torch_function__(func, types, args, kwargs)
