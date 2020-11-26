96c877206e1c19a09627aca4710eaa8cfb75272b
Albert Zeyer <albzey@gmail.com>
2020-11-26 19:33:07 +0100

    max pool modules

--- pytorch_to_returnn/torch/nn/modules/__init__.py
+++ pytorch_to_returnn/torch/nn/modules/__init__.py
@@ -2,6 +2,7 @@
 from .module import Module
 from .container import Sequential
 from .conv import *
+from .pooling import *
 from .padding import *
 from .activation import *
 from .linear import *
@@ -10,6 +11,7 @@ from .norm import *
 from .operator import *
 from .variable import *
 from .conv import __all__ as _conv_all
+from .pooling import __all__ as _pooling_all
 from .padding import __all__ as _padding_all
 from .activation import __all__ as _activation_all
 from .linear import __all__ as _linear_all
@@ -21,7 +23,7 @@ from .variable import __all__ as _variable_all
 
 __all__ = (
     ["Module", "Sequential"] +
-    _conv_all + _padding_all +
+    _conv_all + _pooling_all + _padding_all +
     _activation_all + _linear_all + _dropout_all +
     _norm_all +
     _operator_all + _variable_all)
