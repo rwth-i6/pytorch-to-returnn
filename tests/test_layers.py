099a5a168d799e64dd5c78b361198211b80bb118
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2020-11-27 17:52:04 +0100

    test_conv

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -8,6 +8,29 @@ from pytorch_to_returnn import torch
 from pytorch_to_returnn.converter import verify_torch_and_convert_to_returnn
 
 
+def test_conv():
+  n_in, n_out = 11, 13
+  n_batch, n_time = 3, 7
+
+  def model_func(wrapped_import, inputs: torch.Tensor):
+    if typing.TYPE_CHECKING or not wrapped_import:
+      import torch
+    else:
+      torch = wrapped_import("torch")
+    # {'class': 'transposed_conv', 'from': 'layer2', 'activation': None, 'with_bias': True,
+    #  'n_out': 192, 'filter_size': (10,), 'strides': (5,), 'remove_padding': (3,), 'output_padding': (1,)}
+    model = torch.nn.Conv1d(
+      in_channels=n_in,
+      out_channels=n_out,
+      kernel_size=3,
+      stride=2)
+    return model(inputs)
+
+  rnd = numpy.random.RandomState(42)
+  x = rnd.normal(0., 1., (n_batch, n_in, n_time)).astype("float32")
+  verify_torch_and_convert_to_returnn(model_func, inputs=x)
+
+
 def test_conv_transposed():
   n_in, n_out = 11, 13
   n_batch, n_time = 3, 7
