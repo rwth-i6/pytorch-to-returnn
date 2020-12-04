86a1d72f4fabd148d30a059fe5e5d70547c9ce56
Albert Zeyer <albzey@gmail.com>
2020-12-05 00:53:27 +0100

    check

--- pytorch_to_returnn/naming/call.py
+++ pytorch_to_returnn/naming/call.py
@@ -109,7 +109,7 @@ class CallEntry:
       layer_name = self.namespace.name
       returnn_net = parent_namespace.returnn_ctx.network
       assert layer_name not in returnn_net.layers
-      if len(self.module.calls) >= 2:
+      if len(self.module.calls) >= 2 and len(list(self.module.module.parameters())) > 0:
         raise NotImplementedError  # would need to set reuse_params ...
       print(f"*** {returnn_net.name}/{layer_name!r} layer dict: {layer_dict}")
 
