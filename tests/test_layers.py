1ae139cbe24526105aa478d0ef398611f5c4d8c4
Peter Vieting <vieting@i6.informatik.rwth-aachen.de>
2020-12-17 18:25:23 +0100

    add test case for fairseq Fp32GroupNorm

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -334,6 +334,41 @@ def test_group_norm():
     verify_torch_and_convert_to_returnn(model_func, inputs=x)
 
 
+def test_fp32_group_norm():
+  n_batch, n_time = 4, 20
+  for num_groups, num_channels in [(1, 5), (5, 5)]:
+
+    def model_func(wrapped_import, inputs: torch.Tensor):
+      if typing.TYPE_CHECKING or not wrapped_import:
+        import torch
+      else:
+        torch = wrapped_import("torch")
+
+      #copy of Fp32GroupNorm from fairseq
+      class Fp32GroupNorm(torch.nn.GroupNorm):
+        def __init__(self, *args, **kwargs):
+          super().__init__(*args, **kwargs)
+
+        def forward(self, input):
+          output = torch.nn.functional.group_norm(
+            input.float(),
+            self.num_groups,
+            self.weight.float() if self.weight is not None else None,
+            self.bias.float() if self.bias is not None else None,
+            self.eps,
+          )
+          return output.type_as(input)
+
+      model = Fp32GroupNorm(num_groups, num_channels)
+      out = model(inputs)
+      return out
+
+    print(f"test for num_groups={num_groups}, num_channels={num_channels}")
+    rnd = numpy.random.RandomState(42)
+    x = rnd.normal(0., 1., (n_batch, num_channels, n_time)).astype("float32")
+    verify_torch_and_convert_to_returnn(model_func, inputs=x)
+
+
 def test_unsqueeze():
   n_in, n_out = 11, 13
   n_batch, n_time = 3, 7
