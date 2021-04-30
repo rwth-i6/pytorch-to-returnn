7666ce345136d7593bb573b5ff9b0d7a9a8b22ea
vieting <45091115+vieting@users.noreply.github.com>
2021-04-30 10:36:29 +0200

    add F.multi_head_attention_forward (#39)

--- pytorch_to_returnn/torch/nn/functional.py
+++ pytorch_to_returnn/torch/nn/functional.py
@@ -7,6 +7,8 @@ In RETURNN, every operation is a layer.
 """
 
 import numpy
+import warnings
+from pytorch_to_returnn import torch
 from typing import Optional, Union, List, Tuple, Dict, TypeVar, Sequence
 from . import modules
 from ..tensor import Tensor
@@ -434,3 +436,239 @@ def dropout(input: Tensor, p: float = 0.5, training: bool = True, inplace: bool 
     return input
   if p > 0.0:
     return modules.Dropout(p=p, inplace=inplace).as_returnn_torch_functional()(input)
+
+
+def multi_head_attention_forward(
+  query: Tensor,
+  key: Tensor,
+  value: Tensor,
+  embed_dim_to_check: int,
+  num_heads: int,
+  in_proj_weight: Tensor,
+  in_proj_bias: Optional[Tensor],
+  bias_k: Optional[Tensor],
+  bias_v: Optional[Tensor],
+  add_zero_attn: bool,
+  dropout_p: float,
+  out_proj_weight: Tensor,
+  out_proj_bias: Optional[Tensor],
+  training: bool = True,
+  key_padding_mask: Optional[Tensor] = None,
+  need_weights: bool = True,
+  attn_mask: Optional[Tensor] = None,
+  use_separate_proj_weight: bool = False,
+  q_proj_weight: Optional[Tensor] = None,
+  k_proj_weight: Optional[Tensor] = None,
+  v_proj_weight: Optional[Tensor] = None,
+  static_k: Optional[Tensor] = None,
+  static_v: Optional[Tensor] = None,
+) -> Tuple[Tensor, Optional[Tensor]]:
+  # pytorch-to-returnn: remove possible `return handle_torch_function(...)` from original implementation
+
+  tgt_len, bsz, embed_dim = query.size()
+  assert embed_dim == embed_dim_to_check
+  # allow MHA to have different sizes for the feature dimension
+  assert key.size(0) == value.size(0) and key.size(1) == value.size(1)
+
+  if isinstance(embed_dim, torch.Tensor):
+    # embed_dim can be a tensor when JIT tracing
+    head_dim = embed_dim.div(num_heads, rounding_mode='trunc')
+  else:
+    head_dim = embed_dim // num_heads
+  assert head_dim * num_heads == embed_dim, "embed_dim must be divisible by num_heads"
+  scaling = float(head_dim) ** -0.5
+
+  if not use_separate_proj_weight:
+    if (query is key or torch.equal(query, key)) and (key is value or torch.equal(key, value)):
+      # self-attention
+      q, k, v = linear(query, in_proj_weight, in_proj_bias).chunk(3, dim=-1)
+
+    elif key is value or torch.equal(key, value):
+      # encoder-decoder attention
+      # This is inline in_proj function with in_proj_weight and in_proj_bias
+      _b = in_proj_bias
+      _start = 0
+      _end = embed_dim
+      _w = in_proj_weight[_start:_end, :]
+      if _b is not None:
+        _b = _b[_start:_end]
+      q = linear(query, _w, _b)
+
+      if key is None:
+        assert value is None
+        k = None
+        v = None
+      else:
+
+        # This is inline in_proj function with in_proj_weight and in_proj_bias
+        _b = in_proj_bias
+        _start = embed_dim
+        _end = None
+        _w = in_proj_weight[_start:, :]
+        if _b is not None:
+          _b = _b[_start:]
+        k, v = linear(key, _w, _b).chunk(2, dim=-1)
+
+    else:
+      # This is inline in_proj function with in_proj_weight and in_proj_bias
+      _b = in_proj_bias
+      _start = 0
+      _end = embed_dim
+      _w = in_proj_weight[_start:_end, :]
+      if _b is not None:
+        _b = _b[_start:_end]
+      q = linear(query, _w, _b)
+
+      # This is inline in_proj function with in_proj_weight and in_proj_bias
+      _b = in_proj_bias
+      _start = embed_dim
+      _end = embed_dim * 2
+      _w = in_proj_weight[_start:_end, :]
+      if _b is not None:
+        _b = _b[_start:_end]
+      k = linear(key, _w, _b)
+
+      # This is inline in_proj function with in_proj_weight and in_proj_bias
+      _b = in_proj_bias
+      _start = embed_dim * 2
+      _end = None
+      _w = in_proj_weight[_start:, :]
+      if _b is not None:
+        _b = _b[_start:]
+      v = linear(value, _w, _b)
+  else:
+    q_proj_weight_non_opt = torch.jit._unwrap_optional(q_proj_weight)
+    len1, len2 = q_proj_weight_non_opt.size()
+    assert len1 == embed_dim and len2 == query.size(-1)
+
+    k_proj_weight_non_opt = torch.jit._unwrap_optional(k_proj_weight)
+    len1, len2 = k_proj_weight_non_opt.size()
+    assert len1 == embed_dim and len2 == key.size(-1)
+
+    v_proj_weight_non_opt = torch.jit._unwrap_optional(v_proj_weight)
+    len1, len2 = v_proj_weight_non_opt.size()
+    assert len1 == embed_dim and len2 == value.size(-1)
+
+    if in_proj_bias is not None:
+      q = linear(query, q_proj_weight_non_opt, in_proj_bias[0:embed_dim])
+      k = linear(key, k_proj_weight_non_opt, in_proj_bias[embed_dim: (embed_dim * 2)])
+      v = linear(value, v_proj_weight_non_opt, in_proj_bias[(embed_dim * 2):])
+    else:
+      q = linear(query, q_proj_weight_non_opt, in_proj_bias)
+      k = linear(key, k_proj_weight_non_opt, in_proj_bias)
+      v = linear(value, v_proj_weight_non_opt, in_proj_bias)
+  q = q * scaling
+
+  if attn_mask is not None:
+    assert (
+      attn_mask.dtype == torch.float32
+      or attn_mask.dtype == torch.float64
+      or attn_mask.dtype == torch.float16
+      or attn_mask.dtype == torch.uint8
+      or attn_mask.dtype == torch.bool
+    ), "Only float, byte, and bool types are supported for attn_mask, not {}".format(attn_mask.dtype)
+    if attn_mask.dtype == torch.uint8:
+      warnings.warn("Byte tensor for attn_mask in nn.MultiheadAttention is deprecated. Use bool tensor instead.")
+      attn_mask = attn_mask.to(torch.bool)
+
+    if attn_mask.dim() == 2:
+      attn_mask = attn_mask.unsqueeze(0)
+      if list(attn_mask.size()) != [1, query.size(0), key.size(0)]:
+        raise RuntimeError("The size of the 2D attn_mask is not correct.")
+    elif attn_mask.dim() == 3:
+      if list(attn_mask.size()) != [bsz * num_heads, query.size(0), key.size(0)]:
+        raise RuntimeError("The size of the 3D attn_mask is not correct.")
+    else:
+      raise RuntimeError("attn_mask's dimension {} is not supported".format(attn_mask.dim()))
+    # attn_mask's dim is 3 now.
+
+  # convert ByteTensor key_padding_mask to bool
+  if key_padding_mask is not None and key_padding_mask.dtype == torch.uint8:
+    warnings.warn(
+      "Byte tensor for key_padding_mask in nn.MultiheadAttention is deprecated. Use bool tensor instead."
+    )
+    key_padding_mask = key_padding_mask.to(torch.bool)
+
+  if bias_k is not None and bias_v is not None:
+    if static_k is None and static_v is None:
+      k = torch.cat([k, bias_k.repeat(1, bsz, 1)])
+      v = torch.cat([v, bias_v.repeat(1, bsz, 1)])
+      if attn_mask is not None:
+        attn_mask = pad(attn_mask, (0, 1))
+      if key_padding_mask is not None:
+        key_padding_mask = pad(key_padding_mask, (0, 1))
+    else:
+      assert static_k is None, "bias cannot be added to static key."
+      assert static_v is None, "bias cannot be added to static value."
+  else:
+    assert bias_k is None
+    assert bias_v is None
+
+  # Unlike the original implementation, we keep batch axis and num_heads separate.
+  # Therefore, reshape q, k and v to (bsz, num_heads, tgt_len, head_dim)
+  q = q.contiguous().view(tgt_len, bsz, num_heads, head_dim).transpose(0, 1).transpose(1, 2)
+  if k is not None:
+    k = k.contiguous().view(-1, bsz, num_heads, head_dim).transpose(0, 1).transpose(1, 2)
+  if v is not None:
+    v = v.contiguous().view(-1, bsz, num_heads, head_dim).transpose(0, 1).transpose(1, 2)
+
+  if static_k is not None:
+    assert static_k.size(0) == bsz * num_heads
+    assert static_k.size(2) == head_dim
+    k = static_k.view(bsz, num_heads, -1, head_dim)  # adapt to separate batch axis
+
+  if static_v is not None:
+    assert static_v.size(0) == bsz * num_heads
+    assert static_v.size(2) == head_dim
+    v = static_v.view(bsz, num_heads, -1, head_dim)  # adapt to separate batch axis
+
+  src_len = k.size(2)  # adapt to separate batch axis
+
+  if key_padding_mask is not None:
+    assert key_padding_mask.size(0) == bsz
+    assert key_padding_mask.size(1) == src_len
+
+  if add_zero_attn:
+    src_len += 1
+    # adapt to separate batch axis
+    k = torch.cat([k, torch.zeros(k.size()[:2] + (1, k.size(3)), dtype=k.dtype, device=k.device)], dim=2)
+    v = torch.cat([v, torch.zeros(v.size()[:2] + (1, v.size(3)), dtype=v.dtype, device=v.device)], dim=2)
+    if attn_mask is not None:
+      attn_mask = pad(attn_mask, (0, 1))
+    if key_padding_mask is not None:
+      key_padding_mask = pad(key_padding_mask, (0, 1))
+
+  # adapt to separate batch axis
+  attn_output_weights = torch.matmul(q, k.transpose(2, 3))
+  assert list(attn_output_weights.size()) == [bsz, num_heads, tgt_len, src_len]
+
+  if attn_mask is not None:
+    # adapt to separate batch axis
+    attn_mask = attn_mask.view(bsz, num_heads, tgt_len, src_len)
+    if attn_mask.dtype == torch.bool:
+      attn_output_weights.masked_fill_(attn_mask, float("-inf"))
+    else:
+      attn_output_weights += attn_mask
+
+  if key_padding_mask is not None:
+    # adapt to separate batch axis
+    attn_output_weights = attn_output_weights.masked_fill(
+      key_padding_mask.unsqueeze(1).unsqueeze(2),
+      float("-inf"),
+    )
+
+  attn_output_weights = softmax(attn_output_weights, dim=-1)
+  attn_output_weights = dropout(attn_output_weights, p=dropout_p, training=training)
+
+  # adapt to separate batch axis
+  attn_output = torch.matmul(attn_output_weights, v)
+  assert list(attn_output.size()) == [bsz, num_heads, tgt_len, head_dim]
+  attn_output = attn_output.transpose(1, 2).transpose(0, 1).contiguous().view(tgt_len, bsz, embed_dim)
+  attn_output = linear(attn_output, out_proj_weight, out_proj_bias)
+
+  if need_weights:
+    # average attention weights over heads
+    attn_output_weights = attn_output_weights.view(bsz, num_heads, tgt_len, src_len)
+    return attn_output, attn_output_weights.sum(dim=1) / num_heads
+  else:
+    return attn_output, None
