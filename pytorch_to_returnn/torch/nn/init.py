46880d48d2c96237a8b58d81aee9894b30504424
Peter Vieting <vieting@i6.informatik.rwth-aachen.de>
2021-07-21 16:43:24 +0200

    update _calculate_fan_in_and_fan_out

--- pytorch_to_returnn/torch/nn/init.py
+++ pytorch_to_returnn/torch/nn/init.py
@@ -21,7 +21,8 @@ def _calculate_fan_in_and_fan_out(tensor: Tensor):
   num_output_fmaps = tensor.size(0)
   receptive_field_size = 1
   if tensor.dim() > 2:
-    receptive_field_size = tensor[0][0].numel()
+    for s in tensor.shape[2:]:
+      receptive_field_size *= s
   fan_in = num_input_fmaps * receptive_field_size
   fan_out = num_output_fmaps * receptive_field_size
 
