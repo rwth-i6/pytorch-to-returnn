be52f9d0a74f6aa555f95dd51dea1c04ccff8593
Peter Vieting <vieting@i6.informatik.rwth-aachen.de>
2022-01-31 16:45:23 +0100

    add test_arange_dyn_unsqueeze_add

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -1412,6 +1412,23 @@ def test_arange_dyn_unsqueeze():
   verify_torch_and_convert_to_returnn(model_func, inputs=x)
 
 
+def test_arange_dyn_unsqueeze_add():
+  n_batch, n_feat = 3, 5
+
+  def model_func(wrapped_import, inputs: torch.Tensor):
+    if typing.TYPE_CHECKING or not wrapped_import:
+      import torch
+    else:
+      torch = wrapped_import("torch")
+    randint = torch.randint(low=0, high=10, size=(inputs.shape[0], 4))
+    arange = torch.arange(inputs.shape[0]).unsqueeze(1)
+    return randint + arange
+
+  rnd = numpy.random.RandomState(42)
+  x = rnd.normal(0., 1., (n_batch, n_feat)).astype("float32")
+  verify_torch_and_convert_to_returnn(model_func, inputs=x)
+
+
 def test_arange_from_lengths():
   n_batch, n_time = 3, 5
 
